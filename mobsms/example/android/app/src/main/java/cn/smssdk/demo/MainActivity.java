5ad1bd3de79ac9dbf585430329efeb8bc7cd05af
heyunfei <heyunfei@mob.com>
2026-08-04 14:22:19 +0800

    1.1.11：demo去掉传appsecret http://j.mob.com/browse/SECVERIFY-5379

--- mobsms/example/android/app/src/main/java/cn/smssdk/demo/MainActivity.java
+++ mobsms/example/android/app/src/main/java/cn/smssdk/demo/MainActivity.java
@@ -14,13 +14,12 @@ import io.flutter.plugins.GeneratedPluginRegistrant;
 public class MainActivity extends FlutterActivity {
 	// 通过Mob console申请获得
 	private static final String MOB_APPKEY = "moba6b6c6d6";
-	private static final String MOB_APPSECRET = "b89d2427a3bc7ad1aea1e1e8c1d36bf3";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     // 初始化SMSSDK
-		MobSDK.init(this, MOB_APPKEY, MOB_APPSECRET);
+		MobSDK.init(this, MOB_APPKEY);
 		MobSDK.submitPolicyGrantResult(true);
   }
 
